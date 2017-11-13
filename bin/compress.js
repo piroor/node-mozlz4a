4f5abcf721f9be69735270923c7e6753cd11c4a1
YUKI Hiroshi <shimoda@clear-code.com>
2017-11-13 15:01:03 +0900

    Add command line utilities

---
+++ bin/compress.js
@@ -0,0 +1,13 @@
+const fs     = require('fs');
+const Buffer = require('buffer').Buffer;
+const mozlz4a = require('mozlz4a');
+
+var source = fs.readFileSync(process.argv[2])
+var compressed = mozlz4a.compress(source);
+var outfile = process.argv[3] || '-';
+if (outfile == '-') {
+  process.stdout.write(compressed);
+}
+else {
+  fs.writeFileSync(outfile, compressed);
+}
---
+++ bin/decompress.js
@@ -0,0 +1,13 @@
+const fs     = require('fs');
+const Buffer = require('buffer').Buffer;
+const mozlz4a = require('mozlz4a');
+
+var source = fs.readFileSync(process.argv[2])
+var decompressed = mozlz4a.decompress(source);
+var outfile = process.argv[3] || '-';
+if (outfile == '-') {
+  process.stdout.write(decompressed);
+}
+else {
+  fs.writeFileSync(outfile, decompressed);
+}
